7cbdc10a97c9acaa0a8b312bf18d8a12942860f9
RV-Spidey <killerkingmega10@gmail.com>
2024-09-18 12:47:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,27 +31,21 @@ function closeAllSubMenus() {
 }
 
 function showVideo(videoId) {
-  console.log('Showing video with ID:', videoId); // Check if function is triggered
+  console.log('Showing video with ID:', videoId);
 
   const videos = document.querySelectorAll('#video-container .video');
 
   videos.forEach(video => {
     video.style.display = 'none';
-    const iframe = video.querySelector('iframe');
-    if (iframe) {
-      iframe.src = ''; // Remove src to stop loading
-    }
   });
 
   const selectedVideo = document.getElementById(videoId);
   if (selectedVideo) {
     selectedVideo.style.display = 'block';
     const iframe = selectedVideo.querySelector('iframe');
-    if (iframe && !iframe.src) {
-      setTimeout(() => {
-        iframe.src = iframe.dataset.src; // Set src from data-src attribute
-        console.log('Setting iframe src to:', iframe.src); // Check if src is set
-      }, 100); // Adjust delay as needed
+    if (iframe) {
+      iframe.src = iframe.dataset.src; // Set src directly from data-src attribute
+      console.log('Setting iframe src to:', iframe.src);
     }
   }
 }
